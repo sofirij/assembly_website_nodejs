e38d3fb9f519f67b1ddb898e924d8f7cc2fe55f9
sofirij <sofjaja539@gmail.com>
2025-12-13 14:18:17 -0400

    Remove post functionality from backend

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,34 +1,10 @@
-// single route to serve the html and process the assembly code
-
+// single route to serve the static files
 const express = require('express');
 const router = express.Router();
-const { spawn } = require('child_process');
 
 router.route('/')
     .get((req, res) => {
         res.sendFile(__dirname + '/../../../dist/index.html');
     })
-    .post((req, res) => {
-        // run the assembler.exe file and pass req data as stdin
-        const child = spawn('exe/assembler.exe');
-        const assemblyCode = req.body.assemblyCode;
-
-        child.stdin.write(assemblyCode);
-        child.stdin.end();
-
-        let binaryCode = '';
-
-        child.stdout.on('data', (data) => {
-            binaryCode += data.toString();
-        });
-
-        child.stdout.on('end', () => {
-            res.json({binaryCode: binaryCode});
-        });
-
-        child.on('error', (err) => {
-            res.status(500).json({ error: err.message });
-        });
-    });
 
 module.exports = router;
